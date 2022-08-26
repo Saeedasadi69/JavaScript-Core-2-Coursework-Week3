47302c4429e3bc9d9080f241315e37e49003d960
Saeed Asadi Naftchali <saeedasadi69@gmail.com>
2022-08-26 16:03:10 +0430

    exercise A - B

--- 1-exercises/A-reading-list/script.js
+++ 1-exercises/A-reading-list/script.js
@@ -1,26 +1,46 @@
 function readingList(books) {
   // Write your code here...
+  let parentElement = document.getElementById("content");
+
+  books.forEach((element) => {
+    let ulElement = document.createElement("ul");
+    let bookTitleElement = document.createElement("li");
+    bookTitleElement.innerHTML = `Book title: ${element.title}`;
+    let bookAuthorElement = document.createElement("li");
+    bookAuthorElement.innerHTML = `Book author: ${element.author}`;
+    let bookImageElement = document.createElement("li");
+    bookImageElement.innerHTML = `<img src="${element.bookCoverImage}" alt="${element.title}" width="120">`;
+    ulElement.appendChild(bookTitleElement);
+    ulElement.appendChild(bookAuthorElement);
+    ulElement.appendChild(bookImageElement);
+    parentElement.appendChild(ulElement);
+    if (element.alreadyRead === false) {
+      ulElement.className = "not-read-yet";
+    } else if (element.alreadyRead === true) {
+      ulElement.className = "already-read";
+    }
+  });
 }
 
 const books = [
   {
-    title: 'The Design of Everyday Things',
-    author: 'Don Norman',
+    title: "The Design of Everyday Things",
+    author: "Don Norman",
     alreadyRead: false,
-    bookCoverImage: 'https://blackwells.co.uk/jacket/l/9780465050659.jpg',
+    bookCoverImage: "https://blackwells.co.uk/jacket/l/9780465050659.jpg",
   },
   {
-    title: 'The Most Human Human',
-    author: 'Brian Christian',
+    title: "The Most Human Human",
+    author: "Brian Christian",
     alreadyRead: true,
     bookCoverImage:
-      'https://images-na.ssl-images-amazon.com/images/I/41m1rQjm5tL._SX322_BO1,204,203,200_.jpg',
+      "https://images-na.ssl-images-amazon.com/images/I/41m1rQjm5tL._SX322_BO1,204,203,200_.jpg",
   },
   {
-    title: 'The Pragmatic Programmer',
-    author: 'Andrew Hunt',
+    title: "The Pragmatic Programmer",
+    author: "Andrew Hunt",
     alreadyRead: true,
-    bookCoverImage: 'https://blackwells.co.uk/jacket/l/9780135957059.jpg',
+    bookCoverImage: "https://blackwells.co.uk/jacket/l/9780135957059.jpg",
   },
 ];
 
--- 1-exercises/B-todo-list/script.js
+++ 1-exercises/B-todo-list/script.js
@@ -1,12 +1,57 @@
 function todoList(todos) {
   // Write your code here...
+  let parentContent = document.getElementById("content");
+  let fieldsetEl = document.createElement("fieldset");
+  let legendEl = document.createElement("legend");
+  legendEl.innerText = "To do list";
+  fieldsetEl.appendChild(legendEl);
+  todos.forEach((element) => {
+    let todoItemDiv = document.createElement("div");
+    todoItemDiv.innerHTML = `<input type="checkbox" id="${element.todo}" name="${element.todo}"> <label for="${element.todo}">${element.todo}</label>`;
+    fieldsetEl.appendChild(todoItemDiv);
+  });
+  parentContent.appendChild(fieldsetEl);
+  // let inputElements = document.querySelectorAll("input");
+
+  parentContent.addEventListener("click", (event) => {
+    const isLabelTag = event.target.tagName === "LABEL";
+    const isInputTag = event.target.tagName === "INPUT";
+    if (isInputTag) {
+      const isChecked = event.target.checked === true;
+      if (isChecked) {
+        event.target.nextElementSibling.style.textDecoration = "line-through";
+      } else {
+        event.target.nextElementSibling.style.textDecoration = "none";
+      }
+    } else if (isLabelTag) {
+      const isDoneLabel = event.target.style.textDecoration === "line-through";
+      if (!isDoneLabel) {
+        event.target.style.textDecoration = "line-through";
+      } else if (isDoneLabel) {
+        event.target.style.textDecoration = "none";
+      }
+    }
+
+    // else if (isInputTag) {
+    //   console.log(event.target.checked);
+    //   const isChecked = event.target.checked === "true";
+    //   if (!isChecked) {
+    //     event.target.checked = "true";
+    //     // console.dir(event.target.nextElementSibling);
+    //     event.target.nextElementSibling.style.textDecoration = "line-through";
+    //   } else if (isChecked) {
+    //     event.target.checked = "false";
+    //     event.target.nextElementSibling.style.textDecoration = "none";
+    //   }
+    // }
+  });
 }
 
 const todos = [
   { todo: "wash the dishes" },
   { todo: "walk the dog" },
   { todo: "learn javascript" },
-  { todo: "go shopping" }
+  { todo: "go shopping" },
 ];
 
 todoList(todos);
